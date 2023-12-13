8aad6d95a4223074d96994312a0338cca9da3c73
Brandon <brandon101morgan@outlook.com>
2023-12-13 15:52:19 -0600

    New page added
    New page added that displays the article alone. News.html simply provides the link to the article now.
    All functions are working properly.
    Admin, editing, URLS and Views
    Date: 12/13/2023

--- My_Blog/news/urls.py
+++ My_Blog/news/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('news/', views.display_blog, name='display_blog'),
+   # path('list/', views.display_post, name='news'),
+#gives path to single page displaying news article 
+    path('article/<int:blog_id>/', views.display_article, name='display_article'),
+
+    
+
+
+    
+
+    
 ]
--- My_Blog/news/views.py
+++ My_Blog/news/views.py
@@ -1,11 +1,16 @@
 import http
-from django.shortcuts import render
+#from inspect import _Object
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Blog
-# Create your views here.
-#def news(request):
- #   return HttpResponse('Welcome to News')
+
 
 def display_blog(request):
     blogs = Blog.objects.all()
     return render(request, 'news/news.html', {'blogs': blogs})
+
+
+#article html file is to show the single post as a full page
+def display_article(request, blog_id): 
+    blog = get_object_or_404(Blog, pk=blog_id) 
+    return render(request, 'news/article.html', {'blog': blog})
